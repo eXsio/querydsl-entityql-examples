2912a1174e3b53342b03b0341ac1c31b480dbb81
eXsio <sdymitrow@gmail.com>
2020-01-11 19:05:28 +0100

    examples impl

--- src/main/java/pl/exsio/querydsl/entityql/examples/Application.java
+++ src/main/java/pl/exsio/querydsl/entityql/examples/Application.java
@@ -2,12 +2,10 @@ package pl.exsio.querydsl.entityql.examples;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
