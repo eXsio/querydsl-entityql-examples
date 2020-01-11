b44d3bbd8d9f371e23ca7adc22b98da30564c190
eXsio <sdymitrow@gmail.com>
2020-01-11 18:36:03 +0100

    maven plugin in jitpack

--- src/main/java/pl/exsio/querydsl/entityql/examples/entity/generated/QOrder.java
+++ src/main/java/pl/exsio/querydsl/entityql/examples/entity/generated/QOrder.java
@@ -34,24 +34,20 @@ public final class QOrder extends QBase<pl.exsio.querydsl.entityql.examples.enti
     id:
     {
       this.id =
-          ((NumberPath<java.lang.Long>)
-              QPathFactory.create(
-                      this,
-                      new QPathConfig(
-                          java.lang.Long.class, java.lang.Long.class, "ORDER_ID", true, 1, -5))
-                  .get());
+          QPathFactory.<NumberPath<java.lang.Long>>create(
+              this,
+              new QPathConfig(java.lang.Long.class, java.lang.Long.class, "ORDER_ID", true, 1, -5));
+
       addMetadata(this.id, QColumnMetadataFactory.create("ORDER_ID", 1, -5, true));
     }
 
     userId:
     {
       this.userId =
-          ((NumberPath<java.lang.Long>)
-              QPathFactory.create(
-                      this,
-                      new QPathConfig(
-                          java.lang.Long.class, java.lang.Long.class, "USER_ID", false, 2, -5))
-                  .get());
+          QPathFactory.<NumberPath<java.lang.Long>>create(
+              this,
+              new QPathConfig(java.lang.Long.class, java.lang.Long.class, "USER_ID", false, 2, -5));
+
       addMetadata(this.userId, QColumnMetadataFactory.create("USER_ID", 2, -5, false));
     }
 
@@ -68,7 +64,9 @@ public final class QOrder extends QBase<pl.exsio.querydsl.entityql.examples.enti
 
       paths.add(this.id);
 
-      this._primaryKey = createPrimaryKey(paths.toArray(new Path[0]));
+      this._primaryKey =
+          this.<pl.exsio.querydsl.entityql.examples.entity.Order>createPrimaryKey(
+              paths.<Path>toArray(new Path[0]));
     }
   }
 
