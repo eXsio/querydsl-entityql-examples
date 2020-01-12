c38a61053ca5a03e9b50e154faac55ea02cd45e7
eXsio <sdymitrow@gmail.com>
2020-01-12 18:30:17 +0100

    comments

--- src/main/java/pl/exsio/querydsl/entityql/examples/configuration/SpringContext.java
+++ src/main/java/pl/exsio/querydsl/entityql/examples/configuration/SpringContext.java
@@ -4,19 +4,17 @@ import com.querydsl.sql.H2Templates;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.SQLTemplates;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pl.exsio.querydsl.entityql.config.EntityQlQueryFactory;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -30,6 +28,10 @@ public class SpringContext {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
+    /**
+     * Entity Manager is needed only for creating and populating H2 database.
+     * For normal use cases its presence is not required.
+     */
     @Bean
     static LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -46,18 +48,25 @@ public class SpringContext {
         return em;
     }
 
+    /**
+     * Regular Spring JDBC Transaction Manager
+     */
     @Bean
-    static PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-        return transactionManager;
+    static PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
+    /**
+     * SQLTemplates implementation that matches your Database
+     */
     @Bean
     SQLTemplates sqlTemplates() {
-        return new H2Templates(); //choose the implementation that matches your database engine
+        return new H2Templates();
     }
 
+    /**
+     * SQLQueryFactory - main entry point to build and execute SQL queries
+     */
     @Bean
     static SQLQueryFactory queryFactory(DataSource dataSource, SQLTemplates sqlTemplates) {
         return new EntityQlQueryFactory(
